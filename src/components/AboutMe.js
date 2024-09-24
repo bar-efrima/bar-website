3526f4614f5197992e0267c061be4932058cbddc
Bar Efrima <bar.efrima@post.runi.ac.il>
2024-09-24 12:02:59 +0300

    Fade in effect

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './AboutMe.css';
 import myPhoto from '../assets/images/my_photo.png';
 
 function AboutMe() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const aboutSection = document.getElementById('aboutTag');
+      if (aboutSection) {
+        const sectionPosition = aboutSection.getBoundingClientRect().top;
+        const screenPosition = window.innerHeight / 1.1; // the ratio to control when fade-in triggers
+
+        if (sectionPosition < screenPosition) {
+          setIsVisible(true);
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <section className="about-me" id = "aboutTag"> 
-    
-      <a name="aboutmeTag"></a>
+    <section className="about-me" id="aboutTag"> 
       <div className="main-about-me">
         <img src={myPhoto} alt="Bar Efrima" />
         <div className="about-me-text">
           <h2>About Me</h2>
-        
-            <h1 className="letMe">Let me introduce myself.</h1>
-            <p>
-              Bar Efrima, a 25-year-old BSc. Computer Science student,
-              passionate about innovation, AI, and problem-solving. 
-              With programming and graphic design expertise, I bring a unique
-              blend of creativity and technical skills.
-              I research computer-human interaction at Milab lab, Blockchain at Caimbridge University and have a
-              keen interest in mobile and web devleopment.
-              My goal is to push technology's boundaries through new
-              challenges.
-            </p>
+          <h1 className="letMe">Let me introduce myself.</h1>
+          <p className={isVisible ? 'fade-in' : ''}>
+            Bar Efrima, a 25-year-old BSc. Computer Science student,
+            passionate about innovation, AI, and problem-solving. 
+            With programming and graphic design expertise, I bring a unique
+            blend of creativity and technical skills.
+            I research computer-human interaction at Milab lab, Blockchain at Cambridge University and have a
+            keen interest in mobile and web development.
+            My goal is to push technology's boundaries through new
+            challenges.
+          </p>
         </div>
       </div>
     </section>
