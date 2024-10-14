20dbae9c53afa3f732e2af4fb02a613c21e4f6e8
Bar Efrima <bar.efrima@post.runi.ac.il>
2024-10-14 12:58:15 +0300

    Googel Analytics

--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
-
-import Home from './components/Home';
-import Kitchef from './pages/Kitchef_Page/Kitchef';
-import Amazon from './pages/Amazon_Page/Amazon';
-import Unavigate from './pages/Unavigate_Page/Unavigate';
-import Pokemon from './pages/Pokemon_Page/Pokemon';
-import Highlights from './pages/GameHighlights_Page/Highlights';
-import NFT from './pages/NFT_Page/NFT';
-import IoT from './pages/IoT_Page/IoT';
-import Applications from './pages/Applications_Page/Applications';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home'; // Home contains all sections
+import Kitchef from './pages/Kitchef_Page/Kitchef'; // Kitchef project page
+import Amazon from './pages/Amazon_Page/Amazon'; // Kitchef project page
+import Unavigate from './pages/Unavigate_Page/Unavigate'; // Unavigate project page
+import Pokemon from './pages/Pokemon_Page/Pokemon'; // Pokemon project page
+import Highlights from './pages/GameHighlights_Page/Highlights'; // Highlights project page
+import NFT from './pages/NFT_Page/NFT'; // NFT project page
+import IoT from './pages/IoT_Page/IoT'; // IoT project page
+import Applications from './pages/Applications_Page/Applications'; // Mobile applications project page
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-// Custom hook to track page views on route changes
-function usePageTracking() {
-  const location = useLocation();
-
-  useEffect(() => {
-    if (typeof window.gtag === 'function') {
-      window.gtag('config', 'G-RWY26598QH', {
-        page_path: location.pathname + location.search,
-      });
-    }
-  }, [location]);
-}
 
 function App() {
-  usePageTracking(); // Use page tracking hook
-
   return (
+    
     <Router basename="/">
       <Header />
       <Routes>
