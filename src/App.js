95d26c15967a52f51560d90141c67b33e21972a9
Bar Efrima <bar.efrima@post.runi.ac.il>
2024-10-14 12:25:24 +0300

    Debug Mode for google analythics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import ReactGA from 'react-ga';
+
 import Home from './components/Home'; // Home contains all sections
 import Kitchef from './pages/Kitchef_Page/Kitchef'; // Kitchef project page
 import Amazon from './pages/Amazon_Page/Amazon'; // Kitchef project page
@@ -13,10 +16,26 @@ import Footer from './components/Footer';
 
 
 function App() {
+  // Initialize Google Analytics once when the app starts
+  useEffect(() => {
+    ReactGA.initialize('G-RWY26598QH', { debug: true });
+ // Google Analytics Tracking ID
+  }, []);
+
+  // A custom hook to track page views on route changes
+  function usePageTracking() {
+    const location = useLocation();
+
+    useEffect(() => {
+      ReactGA.pageview(location.pathname + location.search);
+    }, [location]);  // Track page view whenever the location changes
+  }
+
   return (
     
     <Router basename="/">
       <Header />
+      <PageTrackingWrapper /> {/* Enable page tracking */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/kitchef" element={<Kitchef />} />
@@ -31,6 +50,12 @@ function App() {
       <Footer />
     </Router>
   );
+}
+
+// A wrapper component to enable page tracking on route changes
+function PageTrackingWrapper() {
+  usePageTracking();
+  return null;  // This component doesn't render anything visible
 }
 
 export default App;
