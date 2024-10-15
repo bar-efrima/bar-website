953f59b5c4c4e5228a562ca33350a82eb06f18e4
Bar Efrima <bar.efrima@post.runi.ac.il>
2024-10-15 16:33:31 +0300

    Add redirect rule for React Router 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // Use HashRouter
 import Home from './components/Home'; // Home contains all sections
 import Kitchef from './pages/Kitchef_Page/Kitchef'; // Kitchef project page
-import Amazon from './pages/Amazon_Page/Amazon'; // Kitchef project page
+import Amazon from './pages/Amazon_Page/Amazon'; // Amazon project page
 import Unavigate from './pages/Unavigate_Page/Unavigate'; // Unavigate project page
 import Pokemon from './pages/Pokemon_Page/Pokemon'; // Pokemon project page
 import Highlights from './pages/GameHighlights_Page/Highlights'; // Highlights project page
@@ -11,11 +11,9 @@ import Applications from './pages/Applications_Page/Applications'; // Mobile app
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-
 function App() {
   return (
-    
-    <Router basename="/">
+    <Router> 
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
