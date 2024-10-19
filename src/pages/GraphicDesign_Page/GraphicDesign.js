69267850e54b5dbfe0123e370ca755b8caf0cb56
Bar Efrima <bar.efrima@post.runi.ac.il>
2024-10-19 13:54:54 +0300

    Image Names

--- src/pages/GraphicDesign_Page/GraphicDesign.js
+++ src/pages/GraphicDesign_Page/GraphicDesign.js
@@ -3,23 +3,25 @@ import './GraphicDesign.css'; // Import the CSS file
 import TextCard from '../components/TextCardProject'; // AboutCard component
 
 // Import your Images
-import img1 from './Images/1.png';
-import img2 from './Images/2.jpg';
-import img3 from './Images/3.png';
-import img4 from './Images/4.png';
-import basta from './Images/basta.png';
-import card from './Images/card.jpg';
-import celestial from './Images/celestial.jpg';
-import customCard from './Images/custom_card.jpg';
-import hamal from './Images/hamal.png';
-import hamalRed from './Images/hamal_red.png';
-import michshor from './Images/michshor.png';
-import royalCats from './Images/royalcats.jpg';
-import oketz from './Images/OKETZ.png';
-import interfaces from './Images/interface.jpg';
-import logoMador from './Images/logo_mador.png';
-import himushLoop from './Images/himush_loop_LONG.gif';
-import effect from './Images/effec_nfc.jpg';
+import basta from './Images/Basta.png';
+import nftCard from './Images/NFT_Card.jpg';
+import celestialCard from './Images/Celestial_Card.jpg';
+import customCard from './Images/Custom_Card.jpg';
+import dogAbilityRed from './Images/Dog_Ability_Red.jpg';
+import dogAbilityYellow from './Images/Dog_Ability_Yellow.png';
+import maorPatch from './Images/Maor_Patch_604.png';
+import nasaPatch from './Images/Nasa_Patch.png';
+import effecNFC from './Images/Effec_NFC.jpg';
+import effectQRCastle from './Images/EffectQR_Castle.jpg';
+import hamalGreen from './Images/Hamal_Green.png';
+import hamalRed from './Images/Hamal_Red.png';
+import michshor from './Images/Michshor.png';
+import royalCats from './Images/Royalcats.jpg';
+import oketz from './Images/Oketz.png';
+import launchDisplay from './Images/Launch_Display.jpg';
+import logoMadorBlack from './Images/Logo_Mador_Black.png';
+import himushLoop from './Images/himush_loop.mp4';
+
 
 function GraphicDesign() {
 
@@ -29,29 +31,30 @@ function GraphicDesign() {
 
   const images = [
 
-    { src: himushLoop, className: 'gallery__item--wide' },
-    { src: interfaces, className: 'gallery__item--wide' },
+  { src: himushLoop, className: 'gallery__item--wide' },
+  { src: launchDisplay, className: 'gallery__item--wide' },
 
-    { src: card, className: 'gallery__item--square' },
-    { src: celestial, className: 'gallery__item--square' },
-    { src: customCard, className: 'gallery__item--square' },
-    { src: royalCats, className: 'gallery__item--square' },
+  { src: nftCard, className: 'gallery__item--square' },
+  { src: celestialCard, className: 'gallery__item--square' },
+  { src: customCard, className: 'gallery__item--square' },
+  { src: royalCats, className: 'gallery__item--square' },
 
-    { src: img3, className: 'gallery__item--square' },
-    { src: img4, className: 'gallery__item--square' },
-    { src: logoMador, className: 'gallery__item--wide' },
- 
-    { src: oketz, className: 'gallery__item--wide' },
-    { src: hamal, className: 'gallery__item--square' },
-    { src: hamalRed, className: 'gallery__item--square' },
+  { src: maorPatch, className: 'gallery__item--square' },
+  { src: nasaPatch, className: 'gallery__item--square' },
+  { src: logoMadorBlack, className: 'gallery__item--wide' },
 
-    { src: michshor, className: 'gallery__item--square' },
+  { src: oketz, className: 'gallery__item--wide' },
+  { src: hamalGreen, className: 'gallery__item--square' },
+  { src: hamalRed, className: 'gallery__item--square' },
 
-    { src: effect, className: 'gallery__item--wide' },
-    { src: img1, className: 'gallery__item--square' },
-    { src: img2, className: 'gallery__item--square' },
+  { src: michshor, className: 'gallery__item--square' },
 
-    { src: basta, className: 'gallery__item--wide' },
+  { src: effecNFC, className: 'gallery__item--wide' },
+  { src: dogAbilityYellow, className: 'gallery__item--square' },
+  { src: dogAbilityRed, className: 'gallery__item--square' },
+
+  { src: basta, className: 'gallery__item--wide' },
+
 
   ];
 
